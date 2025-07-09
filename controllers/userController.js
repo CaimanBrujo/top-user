0d6729685a853add4d6cd5e4b62eb1b59fcb0e50
Caimanbrujo <186770762+CaimanBrujo@users.noreply.github.com>
2025-07-09 17:52:42 +0200

    feat: add user controllers for fetching, inserting and deleting usernames

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const db = require("../db/queries");
+
+// Controller to fetch and display all usernames
+async function getUsernames(req, res) {
+  const { search } = req.query; // Get search parameter from query string
+  try {
+    const usernames = await db.getAllUsernames(search);
+    console.log("Usernames:", usernames);
+    res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
+  } catch (err) {
+    console.error("Error fetching usernames:", err);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+// Controller to render a simple HTML form
+function createUsernameGet(req, res) {
+  res.sendFile(__dirname + "/../views/form.html");
+}
+
+// Controller to handle form submission and save username
+async function createUsernamePost(req, res) {
+  const { username } = req.body;
+  try {
+    console.log("Username to be saved:", username);
+    await db.insertUsername(username);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error inserting username:", err);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+// Controller to delete all usernames from the database
+async function deleteUsernames(req, res) {
+  try {
+    await db.deleteAllUsernames();
+    console.log("All usernames deleted");
+    res.send("All usernames have been deleted.");
+  } catch (err) {
+    console.error("Error deleting usernames:", err);
+    res.status(500).send("Internal Server Error");
+  }
+}
+
+module.exports = {
+  getUsernames,
+  createUsernameGet,
+  createUsernamePost,
+  deleteUsernames,
+};
